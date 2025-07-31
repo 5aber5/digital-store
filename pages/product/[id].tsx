a2e7384c9e6365eede729ca08d5c2c83193ade81
5aber5 <sabersaed2013@gmail.com>
2025-07-31 15:47:05 +0300

    Update [id].tsx

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,1 +1,18 @@
+import { useRouter } from 'next/router';
+import products from '../../data/products';
 
+export default function ProductPage() {
+  const router = useRouter();
+  const { id } = router.query;
+  const product = products.find(p => p.id === Number(id));
+  if (!product) return <div>المنتج غير موجود</div>;
+  return (
+    <div dir="rtl">
+      <h1>{product.name}</h1>
+      <img src={product.image} alt={product.name} style={{ width: 300 }} />
+      <p>{product.description}</p>
+      <p>السعر: {product.price} ريال</p>
+      <button>أضف إلى السلة</button>
+    </div>
+  );
+}
