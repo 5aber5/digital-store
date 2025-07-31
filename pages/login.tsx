0e04ee0861be42246bbe328160a811a4a0f87f87
5aber5 <sabersaed2013@gmail.com>
2025-07-31 15:48:10 +0300

    Update login.tsx

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,1 +1,18 @@
-
+export default function Login() {
+  return (
+    <div dir="rtl">
+      <h1>تسجيل الدخول</h1>
+      <form>
+        <div>
+          <label>البريد الإلكتروني:</label>
+          <input type="email" name="email" />
+        </div>
+        <div>
+          <label>كلمة المرور:</label>
+          <input type="password" name="password" />
+        </div>
+        <button type="submit">دخول</button>
+      </form>
+    </div>
+  );
+}
